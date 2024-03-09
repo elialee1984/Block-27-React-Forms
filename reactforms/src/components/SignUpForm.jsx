12cec77127bdc3de71aacbd4cfbcabf40cd75cf0
Elia Lee <elia.lee1984@gmail.com>
2024-03-09 12:25:03 +0100

    SignUp component complete

--- reactforms/src/components/SignUpForm.jsx
+++ reactforms/src/components/SignUpForm.jsx
@@ -1,9 +1,57 @@
-import React from "react";
+import { useState } from "react";
 
 const SignUpForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/signup",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: username,
+            password: password,
+          }),
+        }
+      );
+      const result = await response.json();
+      console.log("result", result);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
   return (
     <div>
       <h2>Sign up!</h2>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:{" "}
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:{" "}
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
